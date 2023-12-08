409c969ec210138a58caae3ead2469e41391b2c3
lslsls17 <lswht17@gmail.com>
2023-12-07 22:26:52 -0500

    modify data directory

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -14,7 +14,7 @@ import pandas as pd
 import re
 import string
 
-filename = "Womens Clothing E-Commerce Reviews.csv"
+filename = "../data/Womens Clothing E-Commerce Reviews.csv"
 
 # Function to preprocess the text
 def preprocessText(text, token = False):
